5705cf0c8f67e62ad1107bc15585f97502425bb2
Krzysi3k <global.takeover@o2.pl>
2022-09-10 17:36:08 +0200

    added docker endpoint

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"log"
 
+	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v9"
 )
@@ -18,9 +20,16 @@ func main() {
 		DB:       0,
 	})
 
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		log.Panic(err)
+	}
+
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/get-redis-data", GetRedisData(ctx, rdb))
 	r.GET("/redis-info", GetRedisInfo(ctx, rdb))
+	r.GET("/docker-info", GetDockerInfo(ctx, dockerClient))
+
 	r.Run("0.0.0.0:5001")
 }
--- routes.go
+++ routes.go
@@ -3,16 +3,25 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"strings"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v9"
 )
 
+// GET /get-redis-data?data=REDISKEY
 func GetRedisData(ctx context.Context, rdb *redis.Client) gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 		keyName := c.Query("data")
+		if keyName == "" {
+			c.JSON(400, gin.H{"payload": "missing query string"})
+			return
+		}
 		val, err := rdb.Get(ctx, keyName).Result()
 		if err != nil {
 			c.JSON(404, gin.H{"payload": "key not found"})
@@ -32,7 +41,9 @@ func GetRedisData(ctx context.Context, rdb *redis.Client) gin.HandlerFunc {
 	}
 }
 
+// GET /redis-info
 func GetRedisInfo(ctx context.Context, rdb *redis.Client) gin.HandlerFunc {
+
 	return func(c *gin.Context) {
 		keys := []string{
 			"vibration-sensor",
@@ -49,6 +60,40 @@ func GetRedisInfo(ctx context.Context, rdb *redis.Client) gin.HandlerFunc {
 		combinedOutput = append(combinedOutput, map[string]interface{}{"Redis keys-in-use": len(redisKeys)})
 		merged := mergeMaps(combinedOutput...)
 		c.JSON(200, merged)
+	}
+}
+
+// GET /docker-info?items=containers|images
+func GetDockerInfo(ctx context.Context, dockerClient *client.Client) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		queryParam := c.Query("items")
+
+		if queryParam == "containers" {
+			containers, err := dockerClient.ContainerList(ctx, types.ContainerListOptions{All: false})
+			logError(err)
+			conArr := []string{}
+			for _, container := range containers {
+				conName := strings.Replace(container.Names[0], "/", "", -1)
+				line := fmt.Sprintf("%v - %v", conName, container.Status)
+				conArr = append(conArr, line)
+			}
+			c.JSON(200, gin.H{"containers": conArr})
+		} else if queryParam == "images" {
+			images, err := dockerClient.ImageList(ctx, types.ImageListOptions{All: true})
+			logError(err)
+			imgArr := []string{}
+			for _, img := range images {
+				if strings.Contains(img.RepoTags[0], "<none>") {
+					continue
+				}
+				line := fmt.Sprintf("%v, %vMB", img.RepoTags[0], (img.Size / 1024 / 1024))
+				imgArr = append(imgArr, line)
+			}
+			c.JSON(200, gin.H{"images": imgArr})
+		} else {
+			c.JSON(400, gin.H{"payload": "wrong or missing query string"})
+		}
 	}
 }
 
@@ -71,4 +116,10 @@ func mergeMaps(maps ...map[string]interface{}) map[string]interface{} {
 		}
 	}
 	return result
+}
+
+func logError(err error) {
+	if err != nil {
+		log.Println(err)
+	}
 }
